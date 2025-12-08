275e82226de32ccda673c780450707a40958deb9
PCCP-CCCP <2768192103@qq.com>
2025-12-08 22:51:17 +0800

    selfcomputer changes

---
+++ a.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+#include<cmath>
+#include<cstring>
+#include<set>
+#include<queue>
+#include<stack>
+#include<map>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    cout<<s;
+}
